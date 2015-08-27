e3768c17977db1d1fe84356a0b92a0a459d63ddd
Sergiy Shyrkov <sshyrkov@jahia.com>
2015-08-27 15:49:45 +0200

    Moved EncryptionUitls to jahia-commons; introduced PBKDF2 password digester ( QA-7717 )

---
+++ src/main/java/org/jahia/commons/encryption/EncryptionUtils.java
@@ -0,0 +1,178 @@
+/**
+ * ==========================================================================================
+ * =                   JAHIA'S DUAL LICENSING - IMPORTANT INFORMATION                       =
+ * ==========================================================================================
+ *
+ *     Copyright (C) 2002-2015 Jahia Solutions Group SA. All rights reserved.
+ *
+ *     THIS FILE IS AVAILABLE UNDER TWO DIFFERENT LICENSES:
+ *     1/GPL OR 2/JSEL
+ *
+ *     1/ GPL
+ *     ======================================================================================
+ *
+ *     IF YOU DECIDE TO CHOSE THE GPL LICENSE, YOU MUST COMPLY WITH THE FOLLOWING TERMS:
+ *
+ *     "This program is free software; you can redistribute it and/or
+ *     modify it under the terms of the GNU General Public License
+ *     as published by the Free Software Foundation; either version 2
+ *     of the License, or (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program; if not, write to the Free Software
+ *     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ *     As a special exception to the terms and conditions of version 2.0 of
+ *     the GPL (or any later version), you may redistribute this Program in connection
+ *     with Free/Libre and Open Source Software ("FLOSS") applications as described
+ *     in Jahia's FLOSS exception. You should have received a copy of the text
+ *     describing the FLOSS exception, also available here:
+ *     http://www.jahia.com/license"
+ *
+ *     2/ JSEL - Commercial and Supported Versions of the program
+ *     ======================================================================================
+ *
+ *     IF YOU DECIDE TO CHOOSE THE JSEL LICENSE, YOU MUST COMPLY WITH THE FOLLOWING TERMS:
+ *
+ *     Alternatively, commercial and supported versions of the program - also known as
+ *     Enterprise Distributions - must be used in accordance with the terms and conditions
+ *     contained in a separate written agreement between you and Jahia Solutions Group SA.
+ *
+ *     If you are unsure which license is appropriate for your use,
+ *     please contact the sales department at sales@jahia.com.
+ *
+ *
+ * ==========================================================================================
+ * =                                   ABOUT JAHIA                                          =
+ * ==========================================================================================
+ *
+ *     Rooted in Open Source CMS, Jahia's Digital Industrialization paradigm is about
+ *     streamlining Enterprise digital projects across channels to truly control
+ *     time-to-market and TCO, project after project.
+ *     Putting an end to "the Tunnel effect", the Jahia Studio enables IT and
+ *     marketing teams to collaboratively and iteratively build cutting-edge
+ *     online business solutions.
+ *     These, in turn, are securely and easily deployed as modules and apps,
+ *     reusable across any digital projects, thanks to the Jahia Private App Store Software.
+ *     Each solution provided by Jahia stems from this overarching vision:
+ *     Digital Factory, Workspace Factory, Portal Factory and eCommerce Factory.
+ *     Founded in 2002 and headquartered in Geneva, Switzerland,
+ *     Jahia Solutions Group has its North American headquarters in Washington DC,
+ *     with offices in Chicago, Toronto and throughout Europe.
+ *     Jahia counts hundreds of global brands and governmental organizations
+ *     among its loyal customers, in more than 20 countries across the globe.
+ *
+ *     For more information, please visit http://www.jahia.com
+ */
+package org.jahia.commons.encryption;
+
+import org.jasypt.digest.PooledStringDigester;
+import org.jasypt.digest.StringDigester;
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+
+/**
+ * Miscellaneous encryption utilities.
+ * 
+ * @author Sergiy Shyrkov
+ */
+public final class EncryptionUtils {
+
+    private static class SHA1DigesterHolder {
+        static final PooledStringDigester INSTANCE = new PooledStringDigester();
+
+        static {
+            INSTANCE.setAlgorithm("SHA-1");
+            INSTANCE.setSaltSizeBytes(0);
+            INSTANCE.setIterations(1);
+            INSTANCE.setPoolSize(4);
+        }
+    }
+
+    private static class StringEncryptorHolder {
+        static final StandardPBEStringEncryptor INSTANCE = new StandardPBEStringEncryptor();
+
+        static {
+            INSTANCE.setPassword(new String(new byte[] { 74, 97, 104, 105, 97, 32, 120, 67, 77, 32, 54, 46, 53 }));
+        }
+    }
+
+    private static StringDigester getSHA1DigesterLegacy() {
+        return SHA1DigesterHolder.INSTANCE;
+    }
+
+    private static StringEncryptor getStringEncryptor() {
+        return StringEncryptorHolder.INSTANCE;
+    }
+
+    /**
+     * Bi-directional password base decryption of the provided text.
+     * 
+     * @param encrypted
+     *            the text to be decrypted
+     * @return password base decrypted text
+     */
+    public static String passwordBaseDecrypt(String encrypted) {
+        return getStringEncryptor().decrypt(encrypted);
+    }
+
+    /**
+     * Bi-directional password base encryption of the provided text.
+     * 
+     * @param source
+     *            the text to be encrypted
+     * @return password base encrypted text
+     */
+    public static String passwordBaseEncrypt(String source) {
+        return getStringEncryptor().encrypt(source);
+    }
+
+    /**
+     * Creates message digest using the PBKDF2 algorithm.
+     * 
+     * @param source
+     *            the text to be hashed
+     * @return digested text
+     */
+    public static String pbkdf2Digest(String source) {
+        return pbkdf2Digest(source, false);
+    }
+
+    /**
+     * Creates message digest using the PBKDF2 algorithm prefixing the result with a digester ID if requested.
+     * 
+     * @param source
+     *            the text to be hashed
+     * @param prefixWithId
+     *            do we need to prefix the result with a digester ID
+     * @return digested text
+     */
+    public static String pbkdf2Digest(String source, boolean prefixWithId) {
+        PBKDF2Digester digester = PBKDF2Digester.getInstance();
+        return prefixWithId ? digester.getId() + ':' + digester.digest(source) : digester.digest(source);
+    }
+
+    /**
+     * Created the Base64 encoded SHA-1 (no salt) digest of the provided text. The method is introduced for compatibility with the password
+     * encryption in DF prior to 7.1.0.1.
+     * 
+     * @param source
+     *            the source text to be digested
+     * @return the Base64 encoded SHA-1 digest of the provided text
+     */
+    public static String sha1DigestLegacy(String source) {
+        return getSHA1DigesterLegacy().digest(source);
+    }
+
+    /**
+     * Initializes an instance of this class.
+     */
+    private EncryptionUtils() {
+        super();
+    }
+}
---
+++ src/main/java/org/jahia/commons/encryption/PBKDF2Digester.java
@@ -0,0 +1,192 @@
+/**
+ * ==========================================================================================
+ * =                        DIGITAL FACTORY v7.0 - Community Distribution                   =
+ * ==========================================================================================
+ *
+ *     Rooted in Open Source CMS, Jahia's Digital Industrialization paradigm is about
+ *     streamlining Enterprise digital projects across channels to truly control
+ *     time-to-market and TCO, project after project.
+ *     Putting an end to "the Tunnel effect", the Jahia Studio enables IT and
+ *     marketing teams to collaboratively and iteratively build cutting-edge
+ *     online business solutions.
+ *     These, in turn, are securely and easily deployed as modules and apps,
+ *     reusable across any digital projects, thanks to the Jahia Private App Store Software.
+ *     Each solution provided by Jahia stems from this overarching vision:
+ *     Digital Factory, Workspace Factory, Portal Factory and eCommerce Factory.
+ *     Founded in 2002 and headquartered in Geneva, Switzerland,
+ *     Jahia Solutions Group has its North American headquarters in Washington DC,
+ *     with offices in Chicago, Toronto and throughout Europe.
+ *     Jahia counts hundreds of global brands and governmental organizations
+ *     among its loyal customers, in more than 20 countries across the globe.
+ *
+ *     For more information, please visit http://www.jahia.com
+ *
+ * JAHIA'S DUAL LICENSING - IMPORTANT INFORMATION
+ * ============================================
+ *
+ *     Copyright (C) 2002-2015 Jahia Solutions Group SA. All rights reserved.
+ *
+ *     THIS FILE IS AVAILABLE UNDER TWO DIFFERENT LICENSES:
+ *     1/GPL OR 2/JSEL
+ *
+ *     1/ GPL
+ *     ==========================================================
+ *
+ *     IF YOU DECIDE TO CHOSE THE GPL LICENSE, YOU MUST COMPLY WITH THE FOLLOWING TERMS:
+ *
+ *     "This program is free software; you can redistribute it and/or
+ *     modify it under the terms of the GNU General Public License
+ *     as published by the Free Software Foundation; either version 2
+ *     of the License, or (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program; if not, write to the Free Software
+ *     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ *     As a special exception to the terms and conditions of version 2.0 of
+ *     the GPL (or any later version), you may redistribute this Program in connection
+ *     with Free/Libre and Open Source Software ("FLOSS") applications as described
+ *     in Jahia's FLOSS exception. You should have received a copy of the text
+ *     describing the FLOSS exception, and it is also available here:
+ *     http://www.jahia.com/license"
+ *
+ *     2/ JSEL - Commercial and Supported Versions of the program
+ *     ==========================================================
+ *
+ *     IF YOU DECIDE TO CHOOSE THE JSEL LICENSE, YOU MUST COMPLY WITH THE FOLLOWING TERMS:
+ *
+ *     Alternatively, commercial and supported versions of the program - also known as
+ *     Enterprise Distributions - must be used in accordance with the terms and conditions
+ *     contained in a separate written agreement between you and Jahia Solutions Group SA.
+ *
+ *     If you are unsure which license is appropriate for your use,
+ *     please contact the sales department at sales@jahia.com.
+ */
+package org.jahia.commons.encryption;
+
+import java.nio.charset.Charset;
+import java.security.GeneralSecurityException;
+import java.security.MessageDigest;
+
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+
+import org.jasypt.contrib.org.apache.commons.codec_1_3.binary.Base64;
+import org.jasypt.digest.StringDigester;
+import org.jasypt.salt.RandomSaltGenerator;
+
+/**
+ * Digester, that uses Password-Based Key Derivation Function 2 for password hashing.
+ * 
+ * @author Sergiy Shyrkov
+ */
+public class PBKDF2Digester implements StringDigester {
+
+    private static final char HASH_SEPARATOR = '$';
+
+    private static final PBKDF2Digester INSTANCE = new PBKDF2Digester();
+
+    private static final Charset UTF_8 = Charset.forName("UTF-8");
+
+    /**
+     * Returns a singleton instance of this digester.
+     * 
+     * @return a singleton instance of this digester
+     */
+    public static PBKDF2Digester getInstance() {
+        return INSTANCE;
+    }
+
+    private int hashSizeBytes = 32;
+
+    private String id = "p";
+
+    private int iterations = 8192;
+
+    private RandomSaltGenerator saltGenerator;
+
+    private int saltSizeBytes = 64;
+
+    /**
+     * Initializes an instance of this class.
+     */
+    public PBKDF2Digester() {
+        super();
+        this.saltGenerator = new RandomSaltGenerator();
+    }
+
+    public String digest(String message) {
+        byte[] salt = saltGenerator.generateSalt(saltSizeBytes);
+
+        StringBuilder result = new StringBuilder(133);
+
+        return result.append(new String(Base64.encodeBase64(salt, false), UTF_8)).append(HASH_SEPARATOR)
+                .append(hash(message, salt)).toString();
+    }
+
+    private byte[] doHash(String pwd, byte[] salt) {
+        if (pwd == null || pwd.length() == 0) {
+            throw new IllegalArgumentException("Empty passwords are not supported.");
+        }
+
+        SecretKey key = null;
+        try {
+            SecretKeyFactory f = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+            key = f.generateSecret(new PBEKeySpec(pwd.toCharArray(), salt, iterations, hashSizeBytes * 8));
+        } catch (GeneralSecurityException e) {
+            throw new IllegalArgumentException(e);
+        }
+
+        return Base64.encodeBase64(key.getEncoded(), false);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    private String hash(String pwd, byte[] salt) {
+        return new String(doHash(pwd, salt), UTF_8);
+    }
+
+    public boolean matches(String message, String digest) {
+        if (message == null || digest == null) {
+            return false;
+        }
+
+        int pos = digest.indexOf(HASH_SEPARATOR);
+        if (pos == -1) {
+            throw new IllegalArgumentException("Digest of improper format");
+        }
+
+        byte[] salt = Base64.decodeBase64(digest.substring(0, pos).getBytes(UTF_8));
+        byte[] hash = doHash(message, salt);
+
+        return MessageDigest.isEqual(digest.substring(pos + 1).getBytes(UTF_8), hash);
+    }
+
+    public void setHashSizeBytes(int hashSizeBytes) {
+        this.hashSizeBytes = hashSizeBytes;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setIterations(int iterations) {
+        this.iterations = iterations;
+    }
+
+    public void setSaltGenerator(RandomSaltGenerator saltGenerator) {
+        this.saltGenerator = saltGenerator;
+    }
+
+    public void setSaltSizeBytes(int saltSizeBytes) {
+        this.saltSizeBytes = saltSizeBytes;
+    }
+}
